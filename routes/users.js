f9d28eb293e53e71a48ba4e1b9f443885dde32fe
abhishekdhull <46604435+abhishekdhull21@users.noreply.github.com>
2025-05-27 20:27:46 +0530

    db check added

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
+const { checkDBConnection } = require('../config/db');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+    // const status = await checkDBConnection();
+  res.json({done:"l"});
 });
 
 module.exports = router;
